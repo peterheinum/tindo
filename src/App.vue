d60c828da3e58391802816bdf5bb70a80697c770
filimix123 <peter.heinum@gmail.com>
2019-09-08 21:29:21 +0200

    alright we got three graph alternatives going lets go boys

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,117 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- header  -->
+    <Graph :graphData="graphData"/>
+    <!-- router view  -->
+    <!-- footer -->
+    <div class="flex-container">
+      <div class="button" @click="choseData('Matches')">
+        <p>Matches</p>
+      </div>
+      <div class="button" @click="choseData('Likes')">
+        <p>Likes</p>
+      </div>
+      <div class="button" @click="choseData('Passes')">
+        <p>Passes</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Graph from "./components/Graph";
+import router from "vue-router";
+import data from "./assets/data.json";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Graph
+  },
+  data() {
+    return {
+      graphData: [],
+      chartData: {
+        chartType: 'barChart',
+        selector: 'chart',
+        title: 'Important Data',
+        width: 300,
+        height: 200,
+        data: [120, 140, 70, 90, 110, 65, 210]      
+      },
+      charts: []
+    };
+  },
+  methods: {
+    changeData() {
+      const arr = [];
+      for (let i = 0; i < 6; i++) {
+        const random = Math.floor(Math.random() * Math.floor(100));
+        arr.push(random);
+      }
+      this.graphData = arr;
+    },
+    choseData(type) {
+      const usage = data.Usage;
+      const keys = Object.keys(data.Usage)
+      const toArray = obj => Object.keys(obj).map(x => ({ [x]: obj[x] }));
+      const get = (obj, key) => ({ [key]: obj[key] });
+      const objectSize = object => Object.keys(object).length;
+
+      const { app_opens } = get(usage, 'app_opens')
+      const { matches } = get(usage, 'matches')
+      const { messages_received } = get(usage, 'messages_received')
+      const { messages_sent } = get(usage, 'messages_sent')
+      const { swipes_likes } = get(usage, 'swipes_likes')
+      const { swipes_passes } = get(usage, 'swipes_passes')
+      
+      const mass = {
+        Matches: matches,
+        Passes: swipes_passes,
+        Likes: swipes_likes
+      }
+
+      const arr = Object.keys(mass[type]).map(x => mass[type][x])
+      this.graphData = arr
+
+      
+
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.flex-container {
+  display:flex;
+  width:100vw;
+  justify-content:space-around;
+  height:90%;
+}
+.button {
+  background: #3498db;
+  width: 180px;
+  padding: 4px 0;
+  transform: translateX(-50%) translateY(-50%);
+  border-radius: 3px;
+}
+p {
+  font-family: "Roboto";
+  text-align: center;
+  text-transform: uppercase;
+  color: #fff;
+  user-select: none;
+}
+
+p:hover {
+  cursor: pointer;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import VueRouter from 'vue-router'
+import Home from '../src/components/Home'
+import Graph from '../src/components/Graph'
+// import Chart from 'v-chart-plugin'
+ 
+// Vue.use(Chart);
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+const components = [
+  Home,
+  Graph  
+]
+
+Array.prototype.last = function(){
+  return this[this.length-1]
+}
+
+
+const routes = components.map(o => ({path: o.__file.split('/').last().split('.')[0], component: o }) )
+
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
