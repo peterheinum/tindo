903f44b0c76f193155b817e51496ca4aae06e559
filimix123 <peter.heinum@gmail.com>
2019-09-25 22:25:12 +0200

    added filereader thing

---
+++ src/components/Dropzone.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    hey
+    <vue-dropzone ref="myVueDropzone" @vdropzone-sending="removeFile" @vdropzone-file-added="doStuff" id="dropzone" :options="dropzoneOptions"></vue-dropzone>
+  </div>
+</template>
+
+<script>
+import vue2Dropzone from "vue2-dropzone";
+import "vue2-dropzone/dist/vue2Dropzone.min.css";
+export default {
+  name: "app",
+  components: {
+    vueDropzone: vue2Dropzone
+  },
+  mounted() {
+    console.log("yeet");
+  },
+  methods: {
+    doStuff(e) {
+      this.checkFile(e)
+    },
+    checkFile(file) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const json = JSON.parse(e.target.result)
+      };
+      reader.readAsDataURL(file);
+    },
+    removeFile(file) {
+    }
+  },
+  data() {
+    return {
+      dropzoneOptions: {
+        url: "https://httpbin.org/post",
+        thumbnailWidth: 150,
+        maxFilesize: 0.5,
+        headers: { "My-Awesome-Header": "header value" },
+        
+      }
+    };
+  }
+};
+</script>
+
+<style>
+</style>
